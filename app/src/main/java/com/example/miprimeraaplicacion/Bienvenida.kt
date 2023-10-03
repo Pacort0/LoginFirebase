4356a3ef40552bf5449283f836471c3c8912bab2
siu <francisco.rodriguez@iesnervion.es>
2023-10-03 18:50:48 +0200

    vale ahora va mejor creo

--- app/src/main/java/com/example/miprimeraaplicacion/Bienvenida.kt
+++ app/src/main/java/com/example/miprimeraaplicacion/Bienvenida.kt
@@ -7,17 +7,13 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.miprimeraaplicacion.databinding.ActivityBienvenidaBinding
+import com.example.miprimeraaplicacion.databinding.ActivityMainBinding
 
 class Bienvenida : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bienvenida)
-        val usuario = intent.getStringExtra("usuario").toString()
-        Log.i("actividad", "$usuario")
-        val binding = ActivityBienvenidaBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.textoBienvenida.text = "¡Bienvenido $usuario!"
     }
 
     override fun onPause() {
@@ -29,8 +25,13 @@ class Bienvenida : AppCompatActivity() {
     }
 
     override fun onResume() {
-        Log.i("actividad", "resuman")
-        Toast.makeText(applicationContext, "Bienvenido de vuelta", Toast.LENGTH_SHORT).show()
+        val usuario = intent.getStringExtra("usuario").toString()
+        Log.i("actividad", "$usuario")
+        val binding = ActivityBienvenidaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textoBienvenida.text = "¡Bienvenido $usuario!"
+        binding.imageView.visibility = View.INVISIBLE
+        Toast.makeText(applicationContext, "Bienvenido de vuelta, $usuario", Toast.LENGTH_SHORT).show()
         super.onResume()
     }
 
